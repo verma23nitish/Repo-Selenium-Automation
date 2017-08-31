617750cb5d1a412906e7c4a42db338c382d6438b
nitish23verma@gmail.com <nitish23verma@gmail.com>
2017-08-31 21:34:03 +0530

    Second Commit changes done in Demo1.java file and, HelloWorld.jva file.

--- DemoGithub/src/com/git1/Demo1.java
+++ DemoGithub/src/com/git1/Demo1.java
@@ -2,6 +2,8 @@ package com.git1;
 
 public class Demo1 {
 	public static void main(String[] args) {
-		System.out.println("First Demo class file....!!!!");
+		System.out.println("First Demo output....!!!!");
+		
+		System.out.println("Second Demo output....!!!!");
 	}
 }
--- DemoGithub/src/com/git1/HelloWorld.java
+++ DemoGithub/src/com/git1/HelloWorld.java
@@ -3,6 +3,8 @@ package com.git1;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub...1...!!");
+		
+		System.out.println("Hello GitHub...2...!!");
 	}
 
 }
